3423b88c4d27f4b70e0a1d64b6b9ffaa71238b5f
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-10 15:24:04 +0100

    Update InMyMap.java

--- src/InMyMap.java
+++ src/InMyMap.java
@@ -1,16 +1,9 @@
 public interface InMyMap<K,V> {
-    /*put(Object key, Object value) додає пару ключ + значення*/
-    void put(K key, V value);
 
-    //remove(Object key) видаляє пару за ключем
-    void remove(Object key);
+        void put(K key, V value);
+        void remove(Object key);
+        void clear();
+        int size();
+        V get(Object key);
 
-    //clear() очищає колекцію
-    void clear();
-
-    // size() повертає розмір колекції
-    int size();
-
-    //get(Object key) повертає значення (Object value) за ключем
-    V get(Object key);
 }
